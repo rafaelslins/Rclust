25e0c13e1a2b5e7548961f4a198eec4ebe888e47
Rafael da Silva Lins <rdasilvalins@gmail.com>
2019-08-30 00:08:52 -0300

    tests

---
+++ tests/testthat/test-ppclust.R
@@ -0,0 +1,42 @@
+
+library(Rclust)
+library(data.table)
+
+context("Common errors")
+
+test_that("Input", {
+  # data("diflogadenoma")
+  # data <- diflogadenoma[,10:13]
+  # cl <- ppclust(data, alpha = 1e-5)
+  data("diflogcarcinoma")
+  data <- diflogcarcinoma[,38:50]
+  cl <- ppclust(data, alpha = 1e-5)
+  data <- diflogcarcinoma[, 38:39]
+  expect_error(ppclust(data, alpha = 1e-5))
+  data <- diflogcarcinoma[1, 38:50]
+  expect_error(ppclust(data, alpha = 1e-5))
+  expect_error(ppclust("string", alpha = 1e-5))
+  expect_error(ppclust(1:10, alpha = 1e-5))
+  expect_error(ppclust(diflogadenoma, alpha = 1e-5))
+  expect_error(ppclust(diflogadenoma[,-1], alpha = -1))
+  expect_error(ppclust(diflogadenoma[,-1], alpha = 2))
+})
+
+
+test_that("Input2", {
+  data("diflogcarcinoma")
+  data <- diflogcarcinoma[,38:50]
+  cl <- ppclust_h(data, alpha = 1e-5)
+  data <- diflogcarcinoma[, 38:39]
+  expect_error(ppclust_h(data, alpha = 1e-5))
+  data <- diflogcarcinoma[1, 38:50]
+  expect_error(ppclust_h(data, alpha = 1e-5))
+  expect_error(ppclust_h("string", alpha = 1e-5))
+  expect_error(ppclust_h(1:10, alpha = 1e-5))
+  expect_error(ppclust_h(diflogadenoma, alpha = 1e-5))
+  expect_error(ppclust_h(diflogadenoma[,-1], alpha = -1))
+  expect_error(ppclust_h(diflogadenoma[,-1], alpha = 2))
+})
+
+
+
